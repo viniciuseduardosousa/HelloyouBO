10a8b73cbd4cf853ebaa21dd47703907c0d96da6
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-10-08 13:02:08 +0200

    eindopdracht

---
+++ BOopdracht.py
@@ -0,0 +1,73 @@
+from typing import Type
+
+
+print("Hallo ik ben vinicius, de maker van dit applicatie.")
+print("\n dit applicatie gaat over '' en wat er met hem is gebeurt")
+print("")
+print("")
+
+print("wat is jouw naam? ")
+x = input("Type hier jouw naam: " + "aangenaam")
+
+
+print("")
+print("")
+print("\n")
+
+firstLoop = True
+while firstLoop == True:
+    print("")
+    print(" \n")
+
+    print("A. ")
+    print("B. ")
+    print("C. ")
+
+    answer = input("Type hier: ")
+
+    if answer == "A" or "a":
+        print("")
+    elif answer == "B" or "b":
+        print("")
+    elif answer == "C" or "c":
+        print("")
+        firstLoop = False
+
+waaw = True
+while waaw == True:
+    print("")
+    print("")
+
+    print("A. ")
+    print("B. ")
+    print("C. ")
+
+    answer2 = input("Type hier: ")
+
+    if answer2 == "A" or "a":
+        print("")
+    elif answer2 == "B" or "b":
+        print("")
+    elif answer2 == "C" or "c":
+        print("")
+        waaw = False
+
+
+derde = True
+while derde == True:
+    print("")
+    print("")
+    print("")
+
+
+    print("A. ")
+    print("B. ")
+    print("C. ")
+
+    answer3 = input()
+
+    if answer3 == "A" or "a":
+        print("")
+    elif answer3 == "B" or "a":
+        print("")
+    print("")
