bd129b4cfd3196d584b135f89b0d71abbc2c1966
Viniciuseduardosousa <33297@ma-web.nl>
2021-10-28 11:36:15 +0200

    Opdracht klaar

--- BOopdracht.py
+++ BOopdracht.py
@@ -1,8 +1,13 @@
+import os
+import sys
+import time
+
 print("Hallo ik ben vinicius, de maker van dit applicatie.")
 print("\ndit applicatie gaat over Nadja en wat er met haar is gebeurt\n")
 
 print("wat is jouw naam? ")
 naam = input("Type hier jouw naam: ")
+os.system('clear')
 print(f"aangenaam  {naam}\n")
 
 
@@ -12,58 +17,172 @@ print("Oorlog in Aghanistan bestond al jaren. Het begon in 2001 en eindige pas i
 
 firstLoop = True
 while firstLoop == True:
-    print("")
-    print(" \n")
+    print("\nWil je naar Nadja der vcerhaal luisteren? ")
+    print(" \n Ja of nee?")
 
-    print("A. ")
-    print("B. ")
-    print("C. ")
 
     answer = input("Type hier: ")
+    os.system('clear')
 
-    if answer == "A" or "a":
-        print("")
-    elif answer == "B" or "b":
-        print("")
-    elif answer == "C" or "c":
-        print("")
-        firstLoop = False
+    if answer == 'ja':
+        print("ok dus het verhaal ging zo... \n")
+        break
+    elif answer == "nee":
+        print("OK tot ziens.")
+        sys.exit()
+    print("Typ AUB 'ja' of 'nee'")
+        
+        
 
 secondloop = True
 while secondloop == True:
-    print("")
-    print("")
+    print("""Er hing iets in de lucht, die ochtend. 
+    Het was rustiger op straat. 
+    Zelfs ik, als vrouw die in Westerse kleding haar eigen auto bestuurde, werd niet lastiggevallen. 
+    Trucks die normaal werden gebruikt om vuilnis op halen, reden nu propvol politieagenten, ambtenaren en andere mannen in uniform de stad uit. 
+    Ik dacht: wat is hier aan de hand?Mijn baas belde toen ik bijna op mijn werk was, een restaurant op het internationale kamp. 
+    'Ga terug naar huis', zei hij.
+    'Kabul is gevallen.' """)
 
-    print("A. ")
-    print("B. ")
-    print("C. ")
+    print("\n Wil je doorgaan?")
 
-    answer2 = input("Type hier: ")
+    answer2 = input("ja of nee? : ")
 
-    if answer2 == "A" or "a":
-        print("")
-    elif answer2 == "B" or "b":
-        print("")
-    elif answer2 == "C" or "c":
-        print("")
-        secondloop = False
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
 
 
 thirdloop = True
 while thirdloop == True:
-    print("")
-    print("")
-    print("")
+    print("""'Nog geen tien minuten later was de stad gevuld met Taliban. 
+    Mannen met lange haren en baarden: ze waren overal. 
+    In hun ene hand een geweer, in de andere een zweep. 
+    Iedereen werd geslagen. 
+    Mannen, vrouwen en kinderen. 
+    In shock kwam ik aan bij mijn flat. 
+    Daar werd net een bewoner naar buiten gesleurd. 
+    “Het komt goed”, riep de man naar zijn kinderen, die huilden en schreeuwden. 
+    Maar de Taliban schoten de man dood. 
+    Midden op straat, voor de ogen van zijn familie en ons allemaal.'
+""")
+
+    print("\n Wil je doorgaan?")
+
+    answer2 = input("ja of nee? : ")
+
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
+
+fourthloop = True
+while fourthloop == True:
+    print("""'Het voelde alsof de kogel niet alleen die vader raakte, maar mijn hele land. 
+    Op dat moment wist ik dat alles waar we zo hard voor hadden gewerkt, teniet was gedaan. 
+    Vrouwen in Afghanistan werden altijd al achtergesteld. 
+    Ook ik was al op jonge leeftijd slachtoffer van uithuwelijking, kleinering en gruwelijk geweld. 
+    Toch leek er een begin van verandering. 
+    Maar die dag duwde de Taliban ons terug in de tijd. 
+    In hun ogen hebben vrouwen geen rechten, zelfs niet het recht op gedachten of emoties. 
+    Ze zien ons enkel en alleen als babyfabriek.'
+""")
+
+    print("\n Wil je doorgaan?")
+
+    answer2 = input("ja of nee? : ")
+
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
+    
+fifthloop = True
+while fifthloop == True:
+    print("""'Die avond hoorde ik van de Nederlandse ambassade dat ik naar het vliegveld moest komen. 
+    Onderweg zag ik de enorme chaos die daar was uitgebroken. 
+    Na twaalf uur proberen was ik geen stap dichterbij en ging ik terug naar huis. 
+    Ik wilde mijn opties overwegen, maar er was geen tijd. 
+    Mijn huisbaas belde; de Taliban waren langs geweest met een lijst en mijn naam stond erop. 
+    “Ga nu weg, want ze komen je halen”, waarschuwde hij.'
+""")
+
+    print("\n Wil je doorgaan?")
+
+    answer2 = input("ja of nee? : ")
+
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
+    
+    
+sixthloop = True
+while sixthloop == True:
+    print("""'Maar waar kon ik heen? 
+    Mijn familie heeft me al lang geleden in de steek gelaten. 
+    Vrienden en kennissen durfden niet te helpen of waren zelf ook op de vlucht. 
+    Iedereen kende de verhalen over de Taliban, maar de verhalen horen is anders dan hun daden in het echt zien. 
+    Hun wreedheid is meedogenloos. 
+    Er heerste complete paniek. 
+    Uiteindelijk boden onbekenden mij een nacht onderdak.'
+""")
+
+    print("\n Wil je doorgaan?")
+
+    answer2 = input("ja of nee? : ")
+
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
 
 
-    print("A. ")
-    print("B. ")
-    print("C. ")
+seventhloop = True
+while seventhloop == True:
+    print("""'De volgende dag ging ik opnieuw naar het vliegveld. 
+    Naar instructie van de Nederlandse ambassade droeg ik een oranje sjaal en liep ik tot mijn knieën het open riool in. 
+    Daar moest ik, tussen honderden anderen, de aandacht zien te trekken van een Nederlandse soldaat. 
+    We waren wanhopig, angstig en gedroegen ons als wilden. 
+    Urenlang schreeuwde ik mijn longen kapot. 
+    Totdat een Nederlandse soldaat, met een oranje baard, mij zag.'
+""")
 
-    answer3 = input()
+    print("\n Wil je doorgaan?")
 
-    if answer3 == "A" or "a":
-        print("")
-    elif answer3 == "B" or "a":
-        print("")
-        thirdloop == False
+    answer2 = input("ja of nee? : ")
+
+    if answer2 == "ja":
+        break
+    elif answer2 == "nee":
+        print("Ok tot ziens.")
+        sys.exit()
+    print("typ Aub 'Ja' of 'nee'.")
+    
+
+eigthloop = True
+while eigthloop == True:
+    print("""'Ik ben twee keer geboren. 
+    Een keer toen ik op deze wereld ben gezet, en een keer toen die Nederlandse soldaat mij uit het riool omhoogtrok. 
+    De menswaardige manier waarop ik door de Nederlanders daar werd behandeld, zal ik nooit vergeten. 
+    Mijn gevoel was dubbel: ik zou in veiligheid worden gebracht, maar achter mij hoorde ik mensen smeken. 
+    Zelfs nu, een maand later, ben ik soms bang dat ik droom en eigenlijk helemaal niet ben gered. 
+    Als dat zo is, kan ik alleen maar hopen dat ik nooit meer wakker wordt.'
+""")
+    time.sleep(8)
+    print("\n \n DIT WAS HET VERHAAL VAN NADJA BEDANKT VOOR UW TIJD")
+    time.sleep(2)
+    break
+
+    
